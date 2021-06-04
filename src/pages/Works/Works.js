8b5a90a6b5239b7d83b5a6b44e241284681c127c
Edirin Okpikpi <pleasureokpikpi@gmail.com>
2021-06-04 02:05:43 -0500

    added more content

--- src/pages/Works/Works.js
+++ src/pages/Works/Works.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import './Works.css'
 
 function Works() {
     return (
-        <div>
-            <h1>Welcome to Works Page</h1>
+        <div className="workcontainer">
+            <h1>Personal Projects</h1>
+            <div className="project1">
+            <h1>Ecological Areas of Manitoba</h1>
+            <iframe width= "400" height="500" title="Ecological Areas of Manitoba" src="//ed3um6x3suex7ljp.maps.arcgis.com/apps/Embed/index.html?webmap=ba32d417be7c4dc498d82eb298a6c198&extent=-110.0419,47.2588,-84.1362,55.8214&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at consectetur lorem donec massa sapien faucibus et. Non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Aliquam vestibulum. Link here  </p>
+            </div>
+
+            <div className="project2">
+            <h1>Agricultural Terain Mapping</h1>
+            <iframe width= "400" height="500" title="Ecological Areas of Manitoba" src="//ed3um6x3suex7ljp.maps.arcgis.com/apps/Embed/index.html?webmap=ba32d417be7c4dc498d82eb298a6c198&extent=-110.0419,47.2588,-84.1362,55.8214&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at consectetur lorem donec massa sapien faucibus et. Non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Aliquam vestibulum.  </p>
+            </div>
+
+            <div className="project2">
+            <h1>Agricultural Terain Mapping</h1>
+            <iframe width= "400" height="500" title="Ecological Areas of Manitoba" src="//ed3um6x3suex7ljp.maps.arcgis.com/apps/Embed/index.html?webmap=ba32d417be7c4dc498d82eb298a6c198&extent=-110.0419,47.2588,-84.1362,55.8214&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at consectetur lorem donec massa sapien faucibus et. Non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Aliquam vestibulum.  </p>
+            </div>
+              
+              
         </div>
     )
 }
