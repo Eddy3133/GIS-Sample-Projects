9da22732fdfcf95c26c1d32325f00052ad889db7
Edirin Okpikpi <pleasureokpikpi@gmail.com>
2021-06-06 14:45:23 -0500

    stuff

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,14 +6,19 @@ import profile from '../../images/profile.jpg'
 function About() {
     return (
         <div className="aboutcontainer">
-            <div className="aboutheading">
             <div className="aboutimage">
-            <img loading="lazy" src={profile} alt="Profile of Edirin"/>
-            <h1>ABOUT ME</h1>
-            <p>I have more than 4 years of expereince working with and developing GIS products with the use of ESRI's ArcGIS Desktop 10.5 - 10.8, ArcGIS Pro 2.4.1, ArcGIS Online, ArcGIS collector and other GIS software applications line QGIS, Map View and Jupyter (using ArcGIS and Pandas libraries). I have earned Bachelor of Science degree from The Facualty of Applied Computer Science in The University of Winnipeg and I also have a minor in Geography.My GIS development and technical skills comes from both work and school experince and also personal learning because I enjoy learning about GIS.
-                </p>
+            <img style={{ width: "300px" }} src={profile} alt="Pineapple" />
             </div>
+           
+            <div className="aboutinfo">
+            <h1>About Me</h1>
+                <p>
+                    I have more than 4 years of expereince working with and developing GIS products with the use of ESRI's ArcGIS Desktop 10.5 - 10.8, ArcGIS Pro 2.4.1, ArcGIS Online, ArcGIS collector and other GIS software applications line QGIS, Map View and Jupyter (using ArcGIS and Pandas libraries). I have earned Bachelor of Science degree from The Facualty of Applied Computer Science in The University of Winnipeg and I also have a minor in Geography.My GIS development and technical skills comes from both work and school experince and also personal learning because I enjoy learning about GIS.
+              </p>
             </div>
+
+
+
         </div>
     )
 }
